e0f23aa3d46b9425fc27c65a86dafa2594b6fb78
LeftShifter <peper.dr@inbox.ru>
2019-11-11 17:14:52 +0300

    dz

---
+++ dz/app/src/main/java/com/example/dz/N1.java
@@ -0,0 +1,18 @@
+package com.example.dz;
+
+public class N1 {
+    public String Name;
+    public String Surname;
+    public int Yo;
+    public int School;
+    public int Class;
+    public String Gender;
+    public N1(String Name, String Surname, int Yo, int School, int Class, String Gender){
+        this.Name=Name;
+        this.Surname=Surname;
+        this.Yo=Yo;
+        this.School=School;
+        this.Class=Class;
+        this.Gender=Gender;
+    }
+}
